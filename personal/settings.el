1d6602e90dfc35ad1b08c03b76df555427511a8d
James Aley <james.aley@gmail.com>
2013-02-09 15:27:33 +0000

    Adding personal settings to prelude.

---
+++ personal/settings.el
@@ -0,0 +1,14 @@
+;;
+;; Frame settings
+;;
+
+(scroll-bar-mode -1)
+(show-paren-mode 1)
+
+;;
+;; Editing behaviour
+;;
+
+(setq-default indent-tabs-mode nil)
+(setq x-select-enable-clipboard t)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
