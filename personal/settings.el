fe5d07b6f3bbb08cf6e9f285137fd7f7080985a9
James Aley <james.aley@gmail.com>
2013-04-29 11:17:42 +0100

    Add ESS and CLJS mode.

--- personal/settings.el
+++ personal/settings.el
@@ -12,3 +12,7 @@
 (setq-default indent-tabs-mode nil)
 (setq x-select-enable-clipboard t)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(add-to-list 'auto-mode-alist '("\\.cljs" . clojure-mode))
+
+;;; Packages
+(prelude-ensure-module-deps '(ess cljsbuild-mode))
